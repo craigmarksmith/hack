73c114edec4b5f50c974791be908f59a65e0b771
Craig Smith <craigm.smith@au.westfield.com>
2014-05-19 20:02:57 +1000

    Added videos

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,13 +1,31 @@
-function Play(){
+function Play(){};
+
+Play.prototype.setUp = function(){
   var previousId = null;
+  $.getJSON('/shots.json', function(data){
+    previousId = data[data.length-1]['id'];
+  });
+  var me = this;
   setInterval(function(){
     $.getJSON('/shots.json', function(data){
       mostRecentDirection = data[data.length-1];
       if(previousId !=  mostRecentDirection['id']){
         direction = mostRecentDirection['direction'];
         previousId = mostRecentDirection['id'];
+        me.switchVideo(direction);
         console.log(direction);
       }
     });
   }, 500);
+};
+
+Play.prototype.switchVideo = function(direction){
+  var directionVideo = document.getElementById(direction);
+  var waitingVideo = document.getElementById('wait');
+  directionVideo.style.display = 'block';
+  directionVideo.play();
+  setTimeout(function(){
+    directionVideo.style.display = 'none';
+  }, 7000);
+
 };
