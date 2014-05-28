5944ca048309fb464081536a38e4f0a43d0bd04f
Craig Smith <craigm.smith@au.westfield.com>
2014-05-29 07:55:16 +1000

    Added end screen and reset.

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,13 +1,21 @@
-function Play(){
-  this.gameScore = {'me': 0, 'iggy': 0};
-};
+function Play(){};
 
 Play.prototype.setUp = function(){
+  this.gameScore = {'me': 0, 'iggy': 0};
+  var me = this;
+
+  $('#reset').click(function(){
+    me.gameScore = {'me': 0, 'iggy': 0};
+    me.setDisplayScore(me);
+    $('#end').removeClass('show');
+    return false;
+  });
+
   var previousId = null;
   $.getJSON('/shots.json', function(data){
     previousId = data[data.length-1]['id'];
   });
-  var me = this;
+
   setInterval(function(){
     $.getJSON('/shots.json', function(data){
       mostRecentDirection = data[data.length-1];
@@ -20,6 +28,7 @@ Play.prototype.setUp = function(){
         var elementId = direction+'_'+hitOrMiss;
         me.switchVideo(elementId);
         me.updateScore(hitOrMiss, me);
+        me.displayWinnerScreen(me);
       }
     });
   }, 500);
@@ -38,12 +47,11 @@ Play.prototype.switchVideo = function(elementId){
 Play.prototype.updateScore = function(hitOrMiss, me){
   if(hitOrMiss == 'hit'){
     this.gameScore = {me:me.gameScore['me'], iggy:me.gameScore['iggy']+=1};
-    document.getElementById('iscore').innerHTML = me.getScore('iggy', me);
   }
   if(hitOrMiss == 'miss'){
     me.gameScore = {me:me.gameScore['me']+=1, iggy:me.gameScore['iggy']};
-    document.getElementById('cscore').innerHTML = me.getScore('me', me);
   }
+  me.setDisplayScore(me);
 }
 
 Play.prototype.getScore = function(player, me){
@@ -62,22 +70,36 @@ Play.prototype.getScore = function(player, me){
 
 
   if((challenger > 2) && (iggy > 2)){
-  // if both players are over 2
-
     if(thisPlayer == otherPlayer+1){
     // if this player is 1 greater than other player
       return 'Advantage';
-    }
-    if((thisPlayer - otherPlayer) > 1){
+    }else if(thisPlayer == otherPlayer-1){
+      return '40';
+    }else if((thisPlayer - otherPlayer) > 1){
     // if this player > 1 greater than the other player
       return 'Winner';
-    }
-    if(thisPlayer == otherPlayer){
+    }else if(thisPlayer == otherPlayer){
     // if both players are the same
       return 40;
+    }else{
+      return 'Loser'
     }
   }else{
     return scores[me.gameScore[player]];
   }
+}
 
+Play.prototype.setDisplayScore = function(me){
+  document.getElementById('cscore').innerHTML = me.getScore('me', me);
+  document.getElementById('iscore').innerHTML = me.getScore('iggy', me);
+}
+
+Play.prototype.displayWinnerScreen = function(me){
+  if((me.getScore('me', me) == 'Winner') || (me.getScore('iggy', me) == 'Winner')){
+    setTimeout(function(){
+      endScreen = $('#end');
+      endScreen.addClass('show');
+      endScreen.find('result').innerHTML = 'Win!'
+    }, 3000);
+  }
 }
