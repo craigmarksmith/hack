9c1cce8fb3813c10021174a9c43a3e80409d7103
Craig Smith <craigm.smith@au.westfield.com>
2014-05-28 17:35:37 +1000

    Added scoring

--- app/assets/javascripts/play.js
+++ app/assets/javascripts/play.js
@@ -1,4 +1,6 @@
-function Play(){};
+function Play(){
+  this.gameScore = {'me': 0, 'iggy': 0};
+};
 
 Play.prototype.setUp = function(){
   var previousId = null;
@@ -12,16 +14,18 @@ Play.prototype.setUp = function(){
       if(previousId !=  mostRecentDirection['id']){
         direction = mostRecentDirection['direction'];
         previousId = mostRecentDirection['id'];
-        me.switchVideo(direction);
+
+        var options = ['hit', 'miss'];
+        hitOrMiss = options[Math.floor((Math.random() * 2))];
+        var elementId = direction+'_'+hitOrMiss;
+        me.switchVideo(elementId);
+        me.updateScore(hitOrMiss, me);
       }
     });
   }, 500);
 };
 
-Play.prototype.switchVideo = function(direction){
-  var options = ['hit', 'miss']
-  hitOrMiss = options[Math.floor((Math.random() * 2))];
-  var elementId = direction+'_'+hitOrMiss;
+Play.prototype.switchVideo = function(elementId){
   var directionVideo = document.getElementById(elementId);
   var waitingVideo = document.getElementById('wait');
   directionVideo.style.display = 'block';
@@ -29,5 +33,51 @@ Play.prototype.switchVideo = function(direction){
   setTimeout(function(){
     directionVideo.style.display = 'none';
   }, 3000);
-
 };
+
+Play.prototype.updateScore = function(hitOrMiss, me){
+  if(hitOrMiss == 'hit'){
+    this.gameScore = {me:me.gameScore['me'], iggy:me.gameScore['iggy']+=1};
+    document.getElementById('iscore').innerHTML = me.getScore('iggy', me);
+  }
+  if(hitOrMiss == 'miss'){
+    me.gameScore = {me:me.gameScore['me']+=1, iggy:me.gameScore['iggy']};
+    document.getElementById('cscore').innerHTML = me.getScore('me', me);
+  }
+}
+
+Play.prototype.getScore = function(player, me){
+  var scores = [0, 15, 30, 40, "Winner"]
+
+  var iggy = me.gameScore['iggy'];
+  var challenger = me.gameScore['me'];
+
+  var thisPlayer = me.gameScore[player];
+  var otherPlayer;
+  if(player == 'iggy'){
+    otherPlayer = me.gameScore['me'];
+  }else{
+    otherPlayer = me.gameScore['iggy'];
+  }
+
+
+  if((challenger > 2) && (iggy > 2)){
+  // if both players are over 2
+
+    if(thisPlayer == otherPlayer+1){
+    // if this player is 1 greater than other player
+      return 'Advantage';
+    }
+    if((thisPlayer - otherPlayer) > 1){
+    // if this player > 1 greater than the other player
+      return 'Winner';
+    }
+    if(thisPlayer == otherPlayer){
+    // if both players are the same
+      return 40;
+    }
+  }else{
+    return scores[me.gameScore[player]];
+  }
+
+}
